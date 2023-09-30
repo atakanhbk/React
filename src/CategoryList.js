ca54e56455cd506e60d3a7ee00aaac33217c4b5d
atakanhbk <atakanhbk@gmail.com>
2023-09-30 23:31:31 +0300

    Update CategoryList.js

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
-import { ListGroup,ListGroupItem } from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        {
+          categoryId: 1,
+          categoryName: "Beverages",
+        },
+        {
+            categoryId: 2,
+            categoryName: "Condiments",
+        }
+      ],
+    };
+  }
 
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
+        <h3>{this.state.counter}</h3>
         <ListGroup>
           <ListGroupItem>Cras justo odio</ListGroupItem>
           <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
