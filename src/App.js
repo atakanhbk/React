b1bf4fdbf58fd973b97b311e0e233124b922ed2b
atakanhbk <atakanhbk@gmail.com>
2023-10-01 00:03:33 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
 import { Container, Row, Col } from "reactstrap";
 
-function App() {
-  let infoCategory = {title:"Category List"};
-  let infoProduct = {title:"Product List"};
-
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-
-        <Row>
-          <Col xs="3">
-            <CategoryList info={infoCategory} />
-          </Col>
-          <Col xs="9">
-            <ProductList info={infoProduct} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+export default class App extends Component {
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
+  render(){
+    let infoCategory = { title: "Category List" };
+    let infoProduct = { title: "Product List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+  
+          <Row>
+            <Col xs="3">
+              <CategoryList info={infoCategory} />
+            </Col>
+            <Col xs="9">
+              <ProductList info={infoProduct} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  state = {
+    categories: [
+      {
+        categoryId: 1,
+        categoryName: "Beverages",
+      },
+      {
+        categoryId: 2,
+        categoryName: "Condiments",
+      },
+    ],
+    currentCategory: "",
+  };
 
-    state = {
-      categories: [
-        {
-          categoryId: 1,
-          categoryName: "Beverages",
-        },
-        {
-          categoryId: 2,
-          categoryName: "Condiments",
-        },
-      ],
-    };
-
+  
 
   render() {
     return (
@@ -23,9 +24,15 @@ export default class CategoryList extends Component {
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            <ListGroupItem
+              onClick={() => this.changeCategory(category)}
+              key={category.categoryId}
+            >
+              {category.categoryName}
+            </ListGroupItem>
           ))}
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     );
   }
