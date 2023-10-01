efc9f72ec91a5a3af226c5e95ecfe3096ac071d4
atakanhbk <atakanhbk@gmail.com>
2023-10-01 16:45:01 +0300

    Start to add product to basket

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-import { Table } from "reactstrap";
+import { Table , Button } from "reactstrap";
 
 export default class ProductList extends Component {
+  addToCart(product) {
+ 
+    alert("Product added to cart = " + product.productName);
+  }
   render() {
     return (
+
+     
       <div>
         <h3>
           {this.props.info.title}-{this.props.currentCategory}
@@ -16,16 +22,19 @@ export default class ProductList extends Component {
               <th>Quantity Per Unit</th>
               <th>Unit Price</th>
               <th>Units In Stock</th>
+              <th></th>
+    
             </tr>
           </thead>
           <tbody>
             {this.props.products.map((product) => (
               <tr key={product.id}>
-                <th scope="row">{product.id}  </th>
+                <th scope="row">{product.id} </th>
                 <td>{product.productName}</td>
                 <td>{product.quantityPerUnit}</td>
                 <td>{product.unitPrice}</td>
                 <td>{product.unitsInStock}</td>
+                <td><Button onClick={()=> this.addToCart(product)} color="primary">Add</Button></td>
               </tr>
             ))}
           </tbody>
